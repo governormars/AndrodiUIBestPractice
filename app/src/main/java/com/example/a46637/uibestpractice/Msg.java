e7ada262a6114c72967c8f560c28c344c2eb3190
governormars <466375758@qq.com>
2017-11-23 20:47:44 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a46637/uibestpractice/Msg.java
@@ -0,0 +1,30 @@
+package com.example.a46637.uibestpractice;
+
+/**
+ * Created by 46637 on 2017/11/23.
+ */
+
+public class Msg {
+
+    public static final int TYPE_RECEIVED = 0;
+
+    public static final int TYPE_SENT = 1;
+
+    private String content;
+
+    private int type;
+
+    public Msg(String content, int type) {
+        this.content = content;
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+}
